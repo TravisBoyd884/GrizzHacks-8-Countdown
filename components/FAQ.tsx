b22995f94f4fbd40eafcd9ab83c2a4831d5d3c6d
asupetran <ang.supetran@gmail.com>
2025-12-24 17:03:09 -0500

    Unfinished frog

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -4,69 +4,72 @@ import React, { useState } from "react";
 import faqs from "../data/faq.json";
 import FaqElement from "./FaqElement";
 
+
 export default function FAQSection() {
   const [activeFaq, setActiveFaq] = useState<number | null>(null);
   return (
-    <section id="faq" className="min-h-screen">
-      <div className="mx-auto max-w-6xl px-4 py-20">
-        <h2 className="text-2xl font-semibold">FAQ</h2>
+    <>
+      <section id="faq" className="min-h-screen">
+        <div className="mx-auto max-w-6xl px-4 py-20">
+          <h2 className="text-2xl font-semibold">FAQ</h2>
 
-        {/* Desktop View */}
-        <div className="hidden lg:flex flex-row gap-2">
-          {/* left column */}
-          <div className="flex flex-col w-1/2 mt-1 gap-1">
-            {faqs.map((question, index) =>
-              index % 2 === 0 ? (
-                <FaqElement
-                  key={index}
-                  question={question.q}
-                  answer={question.a}
-                  active={index === activeFaq}
-                  show={true}
-                  onToggle={() =>
-                    setActiveFaq(activeFaq === index ? null : index)
-                  } // Toggle active FAQ
-                />
-              ) : null
-            )}
+          {/* Desktop View */}
+          <div className="hidden lg:flex flex-row gap-2">
+            {/* left column */}
+            <div className="flex flex-col w-1/2 mt-1 gap-1">
+              {faqs.map((question, index) =>
+                index % 2 === 0 ? (
+                  <FaqElement
+                    key={index}
+                    question={question.q}
+                    answer={question.a}
+                    active={index === activeFaq}
+                    show={true}
+                    onToggle={() =>
+                      setActiveFaq(activeFaq === index ? null : index)
+                    } // Toggle active FAQ
+                  />
+                ) : null
+              )}
+            </div>
+            {/* right column */}
+            <div className="flex flex-col w-1/2 mt-1 gap-1">
+              {faqs.map((question, index) =>
+                index % 2 !== 0 ? (
+                  <FaqElement
+                    key={index}
+                    question={question.q}
+                    answer={question.a}
+                    active={index === activeFaq}
+                    show={true}
+                    onToggle={() =>
+                      setActiveFaq(activeFaq === index ? null : index)
+                    } // Toggle active FAQ
+                  />
+                ) : null
+              )}
+            </div>
           </div>
-          {/* right column */}
-          <div className="flex flex-col w-1/2 mt-1 gap-1">
-            {faqs.map((question, index) =>
-              index % 2 !== 0 ? (
-                <FaqElement
-                  key={index}
-                  question={question.q}
-                  answer={question.a}
-                  active={index === activeFaq}
-                  show={true}
-                  onToggle={() =>
-                    setActiveFaq(activeFaq === index ? null : index)
-                  } // Toggle active FAQ
-                />
-              ) : null
-            )}
+
+          {/* Mobile View */}
+          <div className="lg:hidden mt-10 space-y-3">
+            {faqs.map((question, index) => (
+              <FaqElement 
+                key={index}
+                question={question.q}
+                answer={question.a}
+                active={index === activeFaq}
+                show={true}
+                onToggle={() =>
+                  setActiveFaq(activeFaq === index ? null : index)
+                } // Toggle active FAQ
+              />
+            
+            ))}
           </div>
-        </div>
 
-        {/* Mobile View */}
-        <div className="lg:hidden mt-10 space-y-3">
-          {faqs.map((question, index) => (
-            <FaqElement 
-              key={index}
-              question={question.q}
-              answer={question.a}
-              active={index === activeFaq}
-              show={true}
-              onToggle={() =>
-                setActiveFaq(activeFaq === index ? null : index)
-              } // Toggle active FAQ
-            />
-          
-          ))}
         </div>
-
-      </div>
-    </section>
+      </section>
+    </>  
   );
 }
--- components/FaqElement.tsx
+++ components/FaqElement.tsx
@@ -21,8 +21,8 @@ const FaqElement: React.FC<FaqElementProps> = ({
     return (
         <>
             <details
-              key={question}
-              className="group rounded-xl border border-white/10 bg-white/5 p-4"
+              className="group rounded-xl border border-white/10 bg-white/5 p-4 z-1"
+              onClick={onToggle}
             >
               <summary className="cursor-pointer list-none font-medium text-white/90">
                 <div className="flex items-center justify-between">
@@ -32,8 +32,20 @@ const FaqElement: React.FC<FaqElementProps> = ({
                   </span>
                 </div>
               </summary>
+            
+            {active && (
               <div className="mt-3 text-sm text-white/70">{answer}</div>
+            )}
             </details>
+
+            {/* frog guy */}
+            <div className="relative">
+                {active && (
+                    <div className="absolute -top-[3rem] right-0 w-20 -z-1">
+                        <img src="/freddy.svg" alt="froggy" className="w-full h-auto"/> {/*might need to fix path */}
+                    </div>
+                )}
+            </div>
         </>
     );
 };
