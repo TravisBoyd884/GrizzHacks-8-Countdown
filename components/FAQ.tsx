186995cd1bc1fd9c162f7e74de76bb009f977bf1
asupetran <ang.supetran@gmail.com>
2025-12-24 15:50:37 -0500

    Created two columns for Q&A

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -11,8 +11,46 @@ export default function FAQSection() {
       <div className="mx-auto max-w-6xl px-4 py-20">
         <h2 className="text-2xl font-semibold">FAQ</h2>
 
+        {/* Desktop View */}
+        <div className="hidden lg:flex flex-row gap-2">
+          {/* left column */}
+          <div className="flex flex-col w-1/2 mt-1 gap-1">
+            {faqs.map((question, index) =>
+              index % 2 === 0 ? (
+                <FaqElement
+                  key={index}
+                  question={question.q}
+                  answer={question.a}
+                  active={index === activeFaq}
+                  show={true}
+                  onToggle={() =>
+                    setActiveFaq(activeFaq === index ? null : index)
+                  } // Toggle active FAQ
+                />
+              ) : null
+            )}
+          </div>
+          {/* right column */}
+          <div className="flex flex-col w-1/2 mt-1 gap-1">
+            {faqs.map((question, index) =>
+              index % 2 !== 0 ? (
+                <FaqElement
+                  key={index}
+                  question={question.q}
+                  answer={question.a}
+                  active={index === activeFaq}
+                  show={true}
+                  onToggle={() =>
+                    setActiveFaq(activeFaq === index ? null : index)
+                  } // Toggle active FAQ
+                />
+              ) : null
+            )}
+          </div>
+        </div>
+
         {/* Mobile View */}
-        <div className="mt-10 space-y-3">
+        <div className="lg:hidden mt-10 space-y-3">
           {faqs.map((question, index) => (
             <FaqElement 
               key={index}
