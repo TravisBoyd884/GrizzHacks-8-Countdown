5abeb98fd66a610ce193df9c1cf6527f5f6d5882
Kaeden Bryer <kaedenbryer@gmail.com>
2025-12-24 20:58:22 -0500

    Add styling and fix frog error

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -9,12 +9,13 @@ export default function FAQSection() {
   const [activeFaq, setActiveFaq] = useState<number | null>(null);
   return (
     <>
-      <section id="faq" className="min-h-screen">
-        <div className="mx-auto max-w-6xl px-4 py-20">
-          <h2 className="text-2xl font-semibold">FAQ</h2>
+      <section id="faq" className="min-h-screen bg-[#023C0F]">
+        <div className="mx-auto w-[80vw] px-4 py-20">
+          {/* Header */}
+          <h2 className="text-[5rem] text-center font-semibold">FAQ's</h2>
 
           {/* Desktop View */}
-          <div className="hidden lg:flex flex-row gap-2">
+          <div className="hidden lg:flex flex-row gap-2 w-full justify-center">
             {/* left column */}
             <div className="flex flex-col w-1/2 mt-1 gap-1">
               {faqs.map((question, index) =>
