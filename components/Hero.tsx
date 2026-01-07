8af4354b54d531265d7c9a30506398eba21b0cc6
MichaelMedulla <156122669+MichaelMedulla@users.noreply.github.com>
2026-01-07 17:53:49 -0500

    Made boat clickable
    Separated the boat from the background SVG and made it clickable and lead to the interest form. Had trouble when it was behind the countdown so I make it so you can click the boat through the countdown.
    Also edited the boat SVG so people know its clickable

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,19 @@
 "use client";
 import dynamic from "next/dynamic";
+import HeroBoat from "./HeroBoat";
 
 const Countdown = dynamic(() => import("./Countdown"), { ssr: false });
 const HeroBGIMage = dynamic(() => import("./HeroBGImage"), { ssr: false });
 
 export default function Hero() {
   return (
-    <section id="home" className="relative min-h-screen overflow-hidden">
+    
+    <section id="home" className="relative min-h-screen overflow-hidden -mb-2">
       <HeroBGIMage />
+      <HeroBoat />
 
       {/* Content */}
-      <div className="mx-auto max-w-6xl px-4 py-16 md:py-30 lg:py-30">
+      <div className="relative z-10 mx-auto max-w-6xl px-4 py-16 pointer-events-none md:py-30 lg:py-30">
         <div className="p-8 text-center sm:text-left">
           <h2 className="text-white/80">Coming March 28th</h2>
           <h1 className="text-3xl font-bold text-white sm:text-5xl">
